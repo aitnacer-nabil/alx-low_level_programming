befdd206025a5751de1e362fbae9ed5b386d5e63
aitnacer-nabil <palancia.gor@gmail.com>
2023-08-05 14:21:27 -0700

    fix betty style

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,43 +1,35 @@
 #include "main.h"
+
 /**
- * cap_string -  a function that capitalizes all words of a string.
+ * cap_string -a function that capitalizes all words of a string.
  * @s : string
  *
- * Return : retun s
+ * Return: retun s
  */
 
 char *cap_string(char *s)
 {
-   	int i = 0;
+	int i = 0;
 
 	while (c[i] != '\0')
-    	{
-		if(
-           	c[i] == ' ' || 
-           	c[i] == '\t' ||
-           	c[i] == '\n' ||
-          	c[i] == ',' ||
-          	c[i] == ';' ||
-          	c[i] == '.' ||
-          	c[i] == '!' ||
-          	c[i] == '?' ||
-          	c[i] == '"' ||
-          	c[i] == '(' ||
-          	c[i] == ')' ||
-          	c[i] == '{' ||
-          	c[i] == '}' )
-       		{
-         
-        	if ( c[i+1] <= 'z' && c[i+1] >= 'a' && c[i+1] > 'A' )
-        	{
-           
-            	c[i+1] = c[i+1] -32;
-          
-        }
-        
-       }
-        i++;
-        
-    }
-    return c;
-}	
+	{
+		if (c[i] == ' ' || c[i] == '\t' || c[i] == '\n' ||
+		c[i] == ',' ||
+		c[i] == '.' ||
+		c[i] == '!' ||
+		c[i] == '?' ||
+		c[i] == '"' ||
+		c[i] == '(' ||
+		c[i] == ')' ||
+		c[i] == '{' ||
+		c[i] == '}')
+		{
+			if (c[i + 1] <= 'z' && c[i + 1] >= 'a' && c[i + 1] > 'A')
+			{
+				c[i + 1] = c[i + 1] - 32;
+			}
+		}
+			i++;
+		}
+		return (c);
+
