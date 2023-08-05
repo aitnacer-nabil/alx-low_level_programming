ef869b078e12e10972c1c5d5174ae253f52f6c04
aitnacer-nabil <palancia.gor@gmail.com>
2023-08-05 11:16:23 -0700

    puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,18 +8,24 @@
 
 void puts_half(char *str)
 {
-	int i, j, half, len;
+	int len, n, i;
 
 	len = 0;
-	for (i = 0; str[i] != '\0'; i++)
+
+	while (str[len] != '\0')
 	{
 		len++;
 	}
-	half = (len - 1) / 2;
-
-	for (j = half; j > len; j++)
+	if (len % 2 == 0)
 	{
-		_putchar(str[j]);
+		n = len / 2;
+	} else
+	{
+		n = (len - 1) / 2;
 	}
-
+	for (i = n; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
