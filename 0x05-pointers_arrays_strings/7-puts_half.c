dc8bfe24da0e199e2d232f0c167e545e58b40759
aitnacer-nabil <palancia.gor@gmail.com>
2023-08-05 11:36:01 -0700

    add one to odd not even

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,7 +10,7 @@ void puts_half(char *str)
 {
 	int len, n, i;
 
-	len = 1;
+	len = 0;
 
 	while (str[len] != '\0')
 	{
@@ -21,7 +21,7 @@ void puts_half(char *str)
 		n = len / 2;
 	} else
 	{
-		n = (len - 1) / 2;
+		n =((len - 1) / 2) +1;
 	}
 	for (i = n; i < len; i++)
 	{
