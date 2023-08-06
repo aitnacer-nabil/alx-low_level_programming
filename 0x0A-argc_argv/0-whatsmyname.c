a110be376a0e6c3c23dfc971b4a55e01abb19ed1
aitnacer-nabil <palancia.gor@gmail.com>
2023-08-06 04:31:22 -0700

    argv and argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - function  entry point
+ * @argc : number of arg
+ * @argv : array of arg
+ *
+ * Return: void
+ */
+
+int main(int argc,char *argv[])
+{
+	if (argc == 0)
+	{
+	}
+	printf("%s \n", argv[0]);
+	return (0);
+}
