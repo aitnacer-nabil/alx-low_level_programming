2676771d72754f552434d7819bd5d5b7604118c8
aitnacer-nabil <palancia.gor@gmail.com>
2023-08-15 13:25:49 -0700

    struct dog

--- 0x0D-preprocessor/0x0E-structures_typedef/dog.h
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
-#ifndef _DOG_H
-#define _DOG_H
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: just a lone dog struct in big kitty world
+ */
 
 struct dog
 {
