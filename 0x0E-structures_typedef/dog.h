5673a515fa9fee696faadd02275bc3e947d5da6f
aitnacer-nabil <palancia.gor@gmail.com>
2023-08-15 13:32:12 -0700

    move file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: just a lone dog struct in big kitty world
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /*_DOG_H */
+
