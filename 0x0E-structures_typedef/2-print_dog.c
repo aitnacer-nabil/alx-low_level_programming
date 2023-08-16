ea3b92f89b4c2ace23de8290a13b7de576c33033
aitnacer-nabil <palancia.gor@gmail.com>
2023-08-16 04:46:31 -0700

    print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -23,6 +23,6 @@ void print_dog(struct dog *d)
 			d->owner = "(nil)";
 		}
 
-		printf("Name: %s\nAge: %.1f\nOwner: %s\n", d->name, d->age, d->owner);
+		printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
 }
